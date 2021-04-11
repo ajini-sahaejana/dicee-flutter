4363a0bf28edbf09df57ebcb46be7ac37785315c
ajini-sahaejana <ajisahejana@gmail.com>
2021-04-12 00:16:10 +0530

    Random Dice Roller
    This will give all the numbers that we could possibly want, and we now have two separate dice, that we can roll on our dice app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,7 +17,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 1;
+  int rightDiceNo = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,17 +33,25 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Left Button Tapped');
+                setState(() {
+                  leftDiceNo = Random().nextInt(6) + 1;
+                  print('Left Button Tapped');
+                  print('diceNumber = $leftDiceNo');
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNo.png'),
             ),
           ),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Right Button Tapped');
+                setState(() {
+                  rightDiceNo = Random().nextInt(6) + 1;
+                  print('Right Button Tapped');
+                  print('diceNumber = $rightDiceNo');
+                });
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNo.png'),
             ),
           ),
         ],
